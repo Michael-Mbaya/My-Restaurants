839da296b51b00052d4ae8b8238687e31c83fc6c
Michael-Mbaya <gikundamike@gmail.com>
2020-10-24 13:27:45 +0300

    unique data nodes, push() before setValue()

--- app/src/main/java/com/example/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/ui/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void saveLocationToFirebase(String location) {
-        mSearchedLocationReference.setValue(location);
+        mSearchedLocationReference.push().setValue(location);
     }
 
 //    private void addToSharedPreferences(String location) {
